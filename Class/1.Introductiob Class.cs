60081e4b1e5b5cb8e04c0e77a677bb5c8163f85c
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 07:47:55 +0700

    Introductiob Class

---
+++ Class/1.Introductiob Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Introductiob_Class
+{   public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0},I am {1}",to,Name);
+        }
+        public static Person parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var person = Person.parse("john");
+            person.Introduce("mosh");
+        }
+    }
+}
