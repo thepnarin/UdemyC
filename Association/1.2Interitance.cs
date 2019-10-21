15aafe52e67a5716a6dd03d49167d759498f0f08
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 08:16:37 +0700

    Interitance

---
+++ Association/1.2Interitance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Interitance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.width = 100;
+            text.Copy();
+        }
+    }
+}
