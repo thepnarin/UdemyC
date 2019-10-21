696aa6a084e1d816830e45dc2e2dd475d1c87c6e
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 07:59:38 +0700

    Indexers

---
+++ Class/7.2Indexers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _8.Indexers
+{
+    public class HttpCookie
+    {
+        private Dictionary<string, string>  _dictionary;
+        public DateTime Expiry { get; set; }
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+        public void SetItem(string key,string value)
+        {
+
+        }
+        public string GetItem(string key)
+        {
+            return null;
+        }
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
