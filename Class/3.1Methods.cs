1839555ab1eeea07acd572fd7a19e5452a9b2f14
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 07:52:32 +0700

    Methods

---
+++ Class/3.1Methods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+        }
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1,2));
+            Console.WriteLine(calculator.Add(1,2,3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0} , {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("point is at ({0},{1})", point.X, point.Y);
+                }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpexted error occured");
+            }
+        }
+    }
+}
