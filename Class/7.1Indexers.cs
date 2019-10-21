2f57436f837e62c33b7a469696a9803a7e48e416
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 07:58:45 +0700

    Indexers

---
+++ Class/7.1Indexers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Mosh";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
