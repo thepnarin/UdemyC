466e58598b04a79a1d6f9192beb8572ed9cbded7
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 07:56:01 +0700

    AccessMosifiers

---
+++ Class/5.AccessMosifiers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.AccessMosifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982,1,1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
