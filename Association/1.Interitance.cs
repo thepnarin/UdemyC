a09d001ccc171f5f941eb44b74ce829a7d3c637a
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 08:15:12 +0700

    Interitance

---
+++ Association/1.Interitance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2.Interitance
+{
+    public class Text : PersentationObject
+    {
+        public Text()
+        {
+        }
+
+        public int FontSize { get; set; }
+        public string FonrName { get; set; }
+        public void Addtyperlink(string url)
+        {
+            Console.WriteLine("we added a link to"+url);
+        }
+    }
+}
