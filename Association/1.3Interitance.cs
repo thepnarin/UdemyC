ec74f5c80722b6f7d5abce8e56e79d01eb58ee0c
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-21 08:17:28 +0700

    Interitance

---
+++ Association/1.3Interitance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2.Interitance
+{
+    public class PersentationObject
+    {
+        public int width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipbord.");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicate.");
+        }
+    }
+}
